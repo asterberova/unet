8d92ddba458976324c4191a5d5e07ab222445580
Adéla Šterberová <asterberova@gmail.com>
2022-02-15 12:30:57 +0100

    grayscale to rgb

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ data_gen_args = dict(rotation_range=0.2,
 myGene = trainGenerator(2,'/data/s2732815/unet/data/train','image','label',data_gen_args,save_to_dir = None)
 
 model = unet()
-model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
+model_checkpoint = ModelCheckpoint('/data/s2732815/unet/data/unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
 model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
 
 testGene = testGenerator("data/s2732815/unet/data/test")
